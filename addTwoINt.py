75b0de8131d972f6b15a7cf8a48f2946624d8fbc
Nathan2413 <nathancarlinot007@gmail.com>
2022-07-24 13:27:34 +0400

    0.2add

--- addTwoINt.py
+++ addTwoINt.py
@@ -1,10 +1,12 @@
 #!/bin/python3
-
 import sys
+
 def add(x,y):
-    return(x+y)
-def main():
-    x=int(sys.argv[1])
-    y=int(sys.argv[2])
-    print (add(x,y))
-main():
+	return(x+y)
+nbr_arg=len(sys.argv)-1
+if (nbr_arg < 2):
+    print("Error")
+
+x = int(sys.argv[1])
+y = int(sys.argv[2])
+print(add(x,y))	
